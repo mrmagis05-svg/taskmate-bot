1792cbd18ee404a3668a99ea918377a8dd4a4059
mrmagis05-svg <mr.magis05@gmail.com>
2026-03-04 19:39:15 +0300

    Update CreateTask.tsx

--- src/pages/CreateTask.tsx
+++ src/pages/CreateTask.tsx
@@ -3,7 +3,7 @@ import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { Calendar, Users, Repeat } from 'lucide-react';
 import { cn } from '../lib/utils';
-import { API_URL } from "./config"
+import { API_URL } from "../config"
 
 
 export default function CreateTask() {
